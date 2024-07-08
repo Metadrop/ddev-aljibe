04fd8c94b663ae0204b1fe3498741c990bb666f2
Juanjo López <juanjo.lopez@metadrop.net>
2024-07-08 12:16:17 +0200

    Added dependencies install script, and redis service.

---
+++ scripts/install-dependencies.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#ddev-generated
+echo "hola"
+ddev get ddev/ddev-adminer
+ddev get Metadrop/ddev-mkdocs
+ddev get Metadrop/ddev-backstopjs
+ddev get Metadrop/ddev-lighthouse
+ddev get Metadrop/ddev-selenium
+ddev get Metadrop/ddev-pa11y
+ddev get Metadrop/ddev-aljibe-assistant
+
+## We should check if memcached is already installed to not install redis in case we are updating.
+if ! ddev get --installed | grep memcached -q; then
+  echo "ddev-memcached no está instalado. Instalando ddev/ddev-redis..."
+  ddev get ddev/ddev-redis
+fi
+
+
