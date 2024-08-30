a32f25ad76a6f50760647d87d5e1ac42141b55b7
Juanjo López <juanjo.lopez@metadrop.net>
2024-08-30 10:20:41 +0200

    ADDONS - Added unlighthouse addon and removed unnecesary code from dependencies install script

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 #ddev-generated
-echo "hola"
+
 ddev get ddev/ddev-adminer
 ddev get Metadrop/ddev-mkdocs
 ddev get Metadrop/ddev-backstopjs
 ddev get Metadrop/ddev-lighthouse
 ddev get Metadrop/ddev-selenium
 ddev get Metadrop/ddev-pa11y
+ddev get Metadrop/ddev-unlighthouse
 ddev get Metadrop/ddev-aljibe-assistant
 
 ## We should check if memcached is already installed to not install redis in case we are updating.
