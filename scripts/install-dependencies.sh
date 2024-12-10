87f68084f942a53f56cb8a03127ec576ba053f74
Juanjo López <juanjo.lopez@metadrop.net>
2024-12-10 16:48:09 +0100

    Replaced ddev get with ddev add-on get command

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 #ddev-generated
 
-ddev get ddev/ddev-adminer
-ddev get Metadrop/ddev-mkdocs
-ddev get Metadrop/ddev-backstopjs
-ddev get Metadrop/ddev-lighthouse
-ddev get Metadrop/ddev-selenium
-ddev get Metadrop/ddev-pa11y
-ddev get Metadrop/ddev-unlighthouse
-ddev get Metadrop/ddev-aljibe-assistant
+ddev add-on get ddev/ddev-adminer
+ddev add-on get Metadrop/ddev-mkdocs
+ddev add-on get Metadrop/ddev-backstopjs
+ddev add-on get Metadrop/ddev-lighthouse
+ddev add-on get Metadrop/ddev-selenium
+ddev add-on get Metadrop/ddev-pa11y
+ddev add-on get Metadrop/ddev-unlighthouse
+ddev add-on get Metadrop/ddev-aljibe-assistant
 
 ## We should check if memcached is already installed to not install redis in case we are updating.
-if ! ddev get --installed | grep memcached -q; then
+if ! ddev add-on get --installed | grep memcached -q; then
   echo "ddev-memcached no está instalado. Instalando ddev/ddev-redis..."
-  ddev get ddev/ddev-redis
+  ddev add-on get ddev/ddev-redis
 fi
 
 
