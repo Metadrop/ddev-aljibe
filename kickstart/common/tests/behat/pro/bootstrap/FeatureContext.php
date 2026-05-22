3aea8dd35a19d6098e90027b6acd268cf211f54a
Ricardo Sanz <ricardo@metadrop.net>
2026-05-22 19:46:47 +0200

    Fix PHPStan errors and remove some steps
    Cookie steps are provided by Metadrop Behat Contexts.
    Permissions during tests should be same as original site se remove that
    option to avoid encouraging it.

--- kickstart/common/tests/behat/pro/bootstrap/FeatureContext.php
+++ kickstart/common/tests/behat/pro/bootstrap/FeatureContext.php
@@ -5,15 +5,14 @@
  * Behat Feature Context file.
  */
 
-use Drupal\DrupalExtension\Context\DrupalContext;
-use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Behat\Context\Environment\InitializedContextEnvironment;
 use Behat\Behat\Hook\Scope\BeforeScenarioScope;
-use Drupal\user\Entity\Role;
+use Drupal\DrupalExtension\Context\DrupalContext;
 
 /**
  * Main project custom context.
  */
-class FeatureContext extends DrupalContext implements SnippetAcceptingContext {
+class FeatureContext extends DrupalContext {
 
   /**
    * Mink context.
@@ -30,56 +29,11 @@ class FeatureContext extends DrupalContext implements SnippetAcceptingContext {
   protected $responsiveContext;
 
   /**
-   * Permissions to grant and revoke on before and after suite.
+   * Custom context parameters.
    *
-   * @var array
-   *
-   * @code
-   * $permissions = [
-   *   "grant" => [
-   *      "edit any article content" => [RoleInterface::ANONYMOUS_ID],
-   *   ],
-   *   "revoke" => [
-   *     "access content" => [RoleInterface::ANONYMOUS_ID],
-   *   ],
-   * ];
+   * @var array<mixed>
    */
-  protected static $permissions = [];
-
-  /**
-   * Sets User permissions.
-   *
-   * @BeforeSuite
-   */
-  public static function beforeSuiteUserPermissionsSet() {
-    $roles = Role::loadMultiple();
-    foreach (self::$permissions as $type => $value) {
-      foreach ($value as $permission => $rolesList) {
-        foreach ($rolesList as $role) {
-          $roles[$role]->{$type . 'Permission'}($permission);
-          $roles[$role]->trustData()->save();
-        }
-      }
-    }
-  }
-
-  /**
-   * Restores User permissions.
-   *
-   * @AfterSuite
-   */
-  public static function afterUserPermissionsRestore() {
-    $roles = Role::loadMultiple();
-    foreach (self::$permissions as $type => $value) {
-      foreach ($value as $permission => $rolesList) {
-        foreach ($rolesList as $role) {
-          $utype = $type == 'grant' ? 'revoke' : 'grant';
-          $roles[$role]->{$utype . 'Permission'}($permission);
-          $roles[$role]->trustData()->save();
-        }
-      }
-    }
-  }
+  protected array $customParameters = [];
 
   /**
    * Retrieve all required sub-contexts.
@@ -88,6 +42,7 @@ class FeatureContext extends DrupalContext implements SnippetAcceptingContext {
    */
   public function gatherContexts(BeforeScenarioScope $scope) {
     $environment = $scope->getEnvironment();
+    assert($environment instanceof InitializedContextEnvironment);
     $this->minkContext = $environment->getContext('Drupal\DrupalExtension\Context\MinkContext');
     $this->responsiveContext = $environment->getContext('NuvoleWeb\Drupal\DrupalExtension\Context\ResponsiveContext');
   }
@@ -100,30 +55,6 @@ class FeatureContext extends DrupalContext implements SnippetAcceptingContext {
    */
   public function __construct($parameters = NULL) {
     $this->customParameters = !empty($parameters) ? $parameters : [];
-  }
-
-  /**
-   * Sets EU Cookie Compliance: Accept.
-   *
-   * @BeforeScenario @cookies-accept
-   */
-  public function beforeSetCookiesAccept() {
-    // The cookie is always sent for the current domain. Since you didn't visit
-    // a page yet, the domain has not been set yet. At the moment there's no way
-    // to choose a domain for a cookie with mink.
-    // You need to make a request before sending a cookie.
-    $this->visitPath('/');
-    $this->getSession()->setCookie("cookie-agreed", "2");
-  }
-
-  /**
-   * Sets EU Cookie Compliance: Reject.
-   *
-   * @BeforeScenario @cookies-reject
-   */
-  public function beforeSetCookiesReject() {
-    $this->visitPath('/');
-    $this->getSession()->setCookie("cookie-agreed", "0");
   }
 
   /**
